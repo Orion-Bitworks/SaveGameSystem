6674ed757ff10e0ec869ff403006e002da3f7a5d
Iris Frades <a24irifragra@inpedralbes.cat>
2025-10-30 18:22:16 +0100

    Enemigo echo ya para que camine solo de manera horizontal

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -8,21 +8,46 @@ public class Enemies : MonoBehaviour
     public int lives;
     private bool isAlive = true;
 
-    // Start is called before the first frame update
-    void Start()
+    public float velocityX, velocityY;
+
+
+    private void Update()
     {
-        
+        MoveEnemy();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetHealth(int lifesRemaining)
     {
-        
+        lives = lifesRemaining;
     }
 
+    public int GetHealth()
+    {
+        return lives;
+    }
+
+    
     private void OnCollisionEnter(Collision collision)
     {
-        lives--;
-        isAlive = false;
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            lives--;
+            isAlive = false;
+            Destroy(gameObject);
+
+            Debug.Log("Vidas restantes:" + lives + "ENEMIGO DESTRUIDO");
+
+        }
+        
+    }
+    
+    void MoveEnemy()
+    {
+        transform.Translate (velocityX * Time.deltaTime, velocityY * Time.deltaTime, 0);
+
+        if((transform.position.x < -9) || (transform.position.x > 9))
+        {
+            velocityX = -velocityX;
+        }
     }
 }
