8ab25fc52d5cafa73df8af8f8dbff6da79b16cdb
GerardBlasco <a24gerblaper@inspedralbes.cat>
2025-11-02 14:50:43 +0100

    Añadido efecto de flotar a las monedas y variables añadidas a CoinData

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 public class CoinController : MonoBehaviour, ISaveable
 {
     [SerializeField] int value = 1;
+    [SerializeField] float movementQuantity = 0.2f;
+    [SerializeField] float movementSpeed = 1.5f;
     [SerializeField] private string id;
     private bool isEnabled = true;
     private CoinData data;
+    private Vector3 posIni;
 
     public string GetUniqueID() => id;
 
@@ -22,7 +25,10 @@ public class CoinController : MonoBehaviour, ISaveable
         CoinData dt = (CoinData) data;
         gameObject.SetActive(dt.enable);
         value = dt.value;
+        movementQuantity = dt.movementQuantity;
+        movementSpeed = dt.movementSpeed;
         transform.position = new Vector3(dt.pos[0], dt.pos[1], dt.pos[2]);
+        posIni = new Vector3(dt.posIni[0], dt.posIni[1], dt.posIni[2]);
         //importante devolver enable a la variable, sino al guardar dos veces se quedara con el enable anterior y puede desaparecer de la nada
         isEnabled = dt.enable;
     }
@@ -30,6 +36,15 @@ public class CoinController : MonoBehaviour, ISaveable
     private void Start()
     {
         id = System.Guid.NewGuid().ToString();
+        posIni = transform.position;
+    }
+
+    private void Update()
+    {
+        FloatingMovement();
+
+        float newY = posIni.y + Mathf.Sin(Time.time * movementSpeed) * movementQuantity;
+        transform.position = new Vector3(posIni.x, newY, posIni.z);
     }
 
     public int GetValue()
@@ -40,6 +55,36 @@ public class CoinController : MonoBehaviour, ISaveable
     public void SetValue(int newValue)
     {
         value = newValue;
+    }
+
+    public float GetMovementQuantity()
+    {
+        return movementQuantity;
+    }
+
+    public void SetMovementQuantity(float newMovementQuantity)
+    {
+        movementQuantity = newMovementQuantity;
+    }
+
+    public float GetMovementSpeed()
+    {
+        return movementSpeed;
+    }
+
+    public void SetMovementSpeed(float newMovementSpeed)
+    {
+        movementSpeed = newMovementSpeed;
+    }
+
+    public Vector3 GetPosIni()
+    {
+        return posIni;
+    }
+
+    public void SetPosIni(Vector3 newPosIni)
+    {
+        posIni = newPosIni;
     }
 
     public bool IsEnabled()
@@ -65,5 +110,10 @@ public class CoinController : MonoBehaviour, ISaveable
         GameManager.instance.AddObtainedCoins(value);
         isEnabled = false;
         gameObject.SetActive(isEnabled);
+    }
+
+    public void FloatingMovement()
+    {
+        
     }
 }
--- Assets/Scripts/CoinData.cs
+++ Assets/Scripts/CoinData.cs
@@ -8,12 +8,18 @@ public class CoinData
 {
     public bool enable;
     public int value;
+    public float movementQuantity;
+    public float movementSpeed;
     public float[] pos;
+    public float[] posIni;
 
     public CoinData(CoinController coin)
     {
         value = coin.GetValue();
+        movementQuantity = coin.GetMovementQuantity();
+        movementSpeed = coin.GetMovementSpeed();
         enable = coin.IsEnabled();
         pos = new float[] { coin.transform.position.x, coin.transform.position.y, coin.transform.position.z };
+        posIni = new float[] { coin.GetPosIni().x, coin.GetPosIni().y, coin.GetPosIni().z };
     }
 }
